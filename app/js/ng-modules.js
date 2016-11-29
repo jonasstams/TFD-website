64b65b6297ccba922d08442c0655f5b3f1625cff
Jonas Stams <jonasstams@gmail.com>
2016-11-29 19:20:13 +0100

    Repository Setup

---
+++ app/js/ng-modules.js
@@ -0,0 +1,8 @@
+var myApp = angular.module('myApp', []);
+myApp.component("infos",{
+      templateUrl: './infos.html'
+  });
+
+myApp.component("customFooter",{
+      templateUrl: './footer.html'
+  });
